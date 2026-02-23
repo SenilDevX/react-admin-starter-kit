2f73a032fde0ec9d36fb92f3f5def3f6bcc15ba9
SenilDevx <senil.mendapara@devxconsultancy.com>
2026-02-23 11:52:07 +0530

    add development badge

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,17 @@
 import { RouterProvider } from 'react-router';
 import { AppProviders } from '@/providers/app-providers';
 import { router } from '@/routes';
+import { Badge } from './components/ui/badge';
 
 export const App = () => {
   return (
-    <AppProviders>
-      <RouterProvider router={router} />
-    </AppProviders>
+    <>
+      <AppProviders>
+        <RouterProvider router={router} />
+      </AppProviders>
+      {import.meta.env.VITE_ENVIRONMENT === 'dev' && (
+        <Badge className="z-30 fixed bottom-16 right-4">Dev</Badge>
+      )}
+    </>
   );
 };
