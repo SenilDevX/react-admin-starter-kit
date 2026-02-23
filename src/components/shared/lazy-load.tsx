205eda28bf124e2088ba8a4a70204b46761a78b1
SenilDevx <senil.mendapara@devxconsultancy.com>
2026-02-23 15:51:23 +0530

    fix spinner position

--- src/components/shared/lazy-load.tsx
+++ src/components/shared/lazy-load.tsx
@@ -2,5 +2,5 @@ import { Suspense } from 'react';
 import { LoadingSpinner } from './loading-spinner';
 
 export const LazyLoad = ({ children }: { children: React.ReactNode }) => (
-  <Suspense fallback={<LoadingSpinner />}>{children}</Suspense>
+  <Suspense fallback={<LoadingSpinner className="h-full" />}>{children}</Suspense>
 );
